b66fbf46520b6a9c0000cacb6782f1db1c25d418
Quentin Calvez <quentin.calvez@quentez.com>
2012-03-22 11:29:18 +0100

    Avancement IHM et renommage.

---
+++ ihm/Mockups/Mockups/Dialogues/SelectionAgent.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mockups
+{
+	/// <summary>
+	/// Interaction logic for SelectionAgent.xaml
+	/// </summary>
+	public partial class SelectionAgent : Window
+	{
+		public SelectionAgent()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
